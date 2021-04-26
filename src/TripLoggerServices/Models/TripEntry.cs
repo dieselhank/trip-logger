099db0c4ff797e938d92fbf4a0e2c6dec39962b5
David Specht <david.specht@agaso.com>
2021-04-26 16:51:26 -0700

    Add Newtonsoft using.

--- src/TripLoggerServices/Models/TripEntry.cs
+++ src/TripLoggerServices/Models/TripEntry.cs
@@ -1,24 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace TripLoggerServices.Models
 {
     public class TripEntry
     {
-        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "id")]
         public Guid Id { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "date")]
+        [JsonProperty(PropertyName = "date")]
         public DateTime Date { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "tripFrom")]
+        [JsonProperty(PropertyName = "tripFrom")]
         public string TripFrom { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "tripTo")]
+        [JsonProperty(PropertyName = "tripTo")]
         public string TripTo { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "description")]
+        [JsonProperty(PropertyName = "description")]
         public string Description { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "distance")]
+        [JsonProperty(PropertyName = "distance")]
         public DistanceDetail Distance { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "createdOn")]
+        [JsonProperty(PropertyName = "createdOn")]
         public DateTime CreatedOn { get; set; }
-        [Newtonsoft.Json.JsonProperty(PropertyName = "modifiedOn")]
+        [JsonProperty(PropertyName = "modifiedOn")]
         public DateTime ModifiedOn { get; set; }
     }
 }
